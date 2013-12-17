2d1325a1e9e5bc7eb653124550dc00fc56d90412
Kamil Kantyka <Kmkantyka@gmail.com>
2013-12-17 18:39:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yellowjacket::Application.config.secret_key_base = 'd28e72e2d92f0674815bda743fd10d3d823136741335771aa23f039784c34d772eb61ee3c5e684006da7a7982c2117a463a3eb3827b3c724d6f6c554245c620e'
